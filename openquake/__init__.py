830cb7c84cd47c7fc80e1573b98ddda2650e666a
Matteo Nastasi <nastasi@openquake.org>
2013-02-08 00:30:05 +0100

    disabled __path__ mangling

--- openquake/__init__.py
+++ openquake/__init__.py
@@ -1,6 +1,6 @@
-import os
-__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-nrmllib")))
-__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-risklib")))
-__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-hazardlib")))
-__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-engine")))
-del os
+# import os
+# __path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-nrmllib/openquake")))
+# __path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-risklib/openquake")))
+# __path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-hazardlib/openquake")))
+# __path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-engine/openquake")))
+# del os
