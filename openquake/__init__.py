4258dc1f1f9922a229af8d5dbd15e5e61aa4f5a5
Matteo Nastasi <nastasi@openquake.org>
2013-02-07 09:55:27 +0100

    openquake/__init__.py added

---
+++ openquake/__init__.py
@@ -0,0 +1,6 @@
+import os
+__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-nrmllib")))
+__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-risklib")))
+__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-hazardlib")))
+__path__.append(os.path.abspath(os.path.join(os.path.dirname(__file__),"../../oq-engine")))
+del os
